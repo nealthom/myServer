04b8f3f344856b082d4ba40e905f17442c3bdec0
nealthom <nealthom@gmail.com>
2020-07-20 02:54:55 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello Express app!");
+});
+
+app.get("/test", (req, res) => {
+  try {
+    res.status(200).send({ msg: "You got it working my brother" });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.listen(3000, () => {
+  console.log("server started");
+});
