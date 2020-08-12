dc0dc03c24537e6aee74ce4e8ae7c9446e86a69f
nealthom <nealthom@gmail.com>
2020-08-12 03:49:16 -0500

    testing routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -13,8 +13,7 @@ router.get("/test", (req, res) => {
 });
 router.post("/test", (req, res) => {
   try {
-    const { name, email, password } = req.body;
-    res.status(200).send({ name, email, password });
+    res.status(200).send(req.body);
   } catch (error) {
     console.log(req);
     res.status(400).send({ error: "sorry bro" });
