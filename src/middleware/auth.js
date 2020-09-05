f00feb831f4d799a0de2b29df3958fa76526c373
nealthom <nealthom@gmail.com>
2020-09-05 02:55:40 -0500

    working on auth

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,14 +2,13 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 const auth = async (req, res, next) => {
-  console.log(req);
   try {
     const token = req.header("Authorization").replace("Bearer ", "");
     const decoded = jwt.verify(token, process.env.jwtSecret);
-
+    console.log(token);
     const user = await User.findOne({
       _id: decoded._id,
-      "tokens.token": token,
+      "tokens.token": token
     });
     if (!user) {
       throw new Error();
@@ -18,9 +17,9 @@ const auth = async (req, res, next) => {
     req.user = user;
     next();
   } catch (error) {
-    res
-      .status(401)
-      .send({ error: `Please authenticate! ${JSON.stringify(req)}` });
+    res.status(401).send({
+      error: `Please authenticate! `
+    });
   }
 };
 
