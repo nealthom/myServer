8a0e8ec1d07bf43aeb32720e763390f988549179
nealthom <nealthom@gmail.com>
2020-09-05 05:58:24 -0500

    update auth middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,30 +1,6 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
-// const auth = async (req, res, next) => {
-//   try {
-//     const token = req.header("Authorization").replace("Bearer ", "");
-//     const decoded = jwt.verify(token, process.env.jwtSecret);
-
-//     const user = await User.findOne({
-//       _id: decoded._id,
-//       "tokens.token": token
-//     });
-//     if (!user) {
-//       throw new Error();
-//     }
-//     req.token = token;
-//     req.user = user;
-//     next();
-//   } catch (error) {
-//     res.status(401).send({
-//       error: `Please authenticate!`
-//     });
-//   }
-// };
-
-// module.exports = auth;
-
 module.exports = async function (req, res, next) {
   const token = req.header("x-auth-token");
 
@@ -38,10 +14,13 @@ module.exports = async function (req, res, next) {
       _id: decoded._id,
       "tokens.token": token
     });
+    if (!user) {
+      throw new Error();
+    }
+    req.token = token;
     req.user = user;
-
     next();
   } catch (err) {
-    res.status(401).json({ msg: "Token is not valid" });
+    res.status(401).json({ msg: "Please Authenticate" });
   }
 };
